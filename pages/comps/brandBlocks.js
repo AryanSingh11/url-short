a16861daae14eed5c82bb49bf8a957862e02ab5d
Aryan Singh <aryansingh22070@gmail.com>
2022-03-04 20:00:24 +0530

    First commit

---
+++ pages/comps/brandBlocks.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Image from 'next/image'
+import styles from '/styles/Home.module.css'
+
+function BrandBlocks() {
+  return (
+    <div>
+        <div className={styles.singleBrandDiv}>
+        <div className={styles.brandLogoDiv}>
+        <Image
+            src="/icon-brand-recognition.svg"
+            width={50}
+            height={50}
+            layout='fixed'
+            background-color='Black'
+        ></Image></div>
+        <h1>Brand Recognition</h1>
+        <p className={styles.brandPara}>Boost your brand recognition with each link.Generic links don't mean a thing.Branded links help instill confidence in your content</p>
+        </div>
+    </div>
+  )
+}
+
+export default BrandBlocks;
